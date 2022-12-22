0781602e1d4cc4ce4dc09026d4ccdcc971996f72
Ramkrish16 <ramrajakrish16@gmail.com>
2022-12-22 14:58:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,139 @@
+// import logo from './logo.svg';
+import './App.css';
+//import img from './tabbg.jpg';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <table bgcolor='white'>
+        <tr background-color='white' >
+          <td>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIaZoZmN__B5SI9Xg9f0liNpNew2_gXTR7rA&usqp=CAU' alt='Logo' height={100} width={100
+            }></img>       
+          </td>
+          <td>
+          <div class="topnav">
+          <a href="#home">Home</a>
+          <a href="#contact">About Us</a>
+          </div>
+          </td>
+        </tr>
+      </table>
+      <table bgcolor='#F0E9E7' width='100%' >
+        <tr align='left' >
+          <td>
+            <h1 align='left'>Complete Python Developer in 2023: Zero to Mastery</h1>
+            <p align='left'>How to become a Python 3 Developer and get hired! Build 12+ projects, learn Web Development, Machine Learning + more!</p><br></br>
+            <p align='left'>Course Instructor:Mr.ABC</p>
+            Star Rating : 
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <br></br><br></br>
+            &ensp;Rs.999&nbsp;<s>Rs.2000</s><br></br>
+            <br></br>
+            &ensp;<button type='button' className='button'>Buy Now</button>
+          </td>
+          <td><br></br>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVg2khdHzaHvCabuq8EMUCJYsE9P1VtL91V1nuj8gkIvFRW3eBUfXZ-Bp7J5sqKS3Tq1Y&usqp=CAU' alt='Course' height={350} width={350} className="img">
+            </img>
+          </td>
+        </tr>
+        
+      </table>
+      <table bgcolor='#F0E9E7 ' width='100%'  >
+      <tr align='left' >
+          <td>
+            <h1 align='left'>What you'll Learn</h1>
+            <ul style={{listStyle:'square',fontWeight:'bold'}}>
+              <li>Become a professional Python Developer and get hired</li><br></br>
+              <li>Learn Object Oriented Programming</li><br></br>
+              <li>Build 12+ real world Python projects you can show off</li><br></br>
+              <li>Learn Machine Learning with Python</li><br></br>
+              <li>Learn Data Science - Analyze and Visualize Data</li><br></br>
+              <li>Use Python to process: Images, CSVs, PDFs, and other Files</li><br></br>         
+            </ul>
+            </td>
+        </tr>
+
+        <tr align='left' >
+          <td>
+            <h1 align='left'>Description</h1>
+            <p>
+             <b>Just updated for 2023! It's time to become a modern and complete Python developer! Join a live online community of over 900,000+ developers and a course taught by an industry expert that has actually worked both in Silicon Valley and Toronto. Graduates of Andrei’s courses are now working at Google, Tesla, Amazon, Apple, IBM, JP Morgan, Meta, + other top tech companies.</b> <br></br>
+
+             Learn Python from scratch, get hired, and have fun along the way with the most modern, up-to-date Python course on Udemy (we use the latest version of Python). This course is focused on efficiency: never spend time on confusing, out of date, incomplete Python tutorials anymore.
+
+This comprehensive and project based course will introduce you ....</p>
+<button type='button' className='button'>Read more</button>
+            
+            </td>
+        </tr>
+      </table>
+
+      <footer>
+      <table bgcolor='#ddd' width='100%'>
+      <tr>
+      <td width="800px" height="50px" colspan="4"></td>
+      </tr>
+      <tr >
+      <td height="200px" width="300px" align="center">
+        <ul style={{listStyle:'none'}} >
+          <li>A2Z business</li><br></br>
+		<li> Teach on A2Z</li><br></br>
+		<li>Get The App</li><br></br>
+		<li> About Us</li><br></br>
+		<li>Investors</li><br></br>
+
+</ul></td>
+<td height="200px" width="300px" align="center"><ul style={{listStyle:'none'}}><li>Carrer</li><br></br>
+		<li> Blog</li><br></br>
+		<li>Help</li><br></br>
+		<li>Affiliate</li><br></br>
+		<li>Accessibility</li><br></br>
+
+</ul></td>
+<td height="200px" width="300px" align="center"><ul style={{listStyle:'none'}} ><li>Terms</li><br></br>
+		<li> Privacy policy and cookie policy</li><br></br>
+		<li>Topics/concepts</li><br></br>
+		<li>sitemap</li><br></br>
+		<li>Features</li><br></br>
+
+</ul></td>	
+	<td height="200px" width="300px" align="center"><ul style={{listStyle:'none'}}><li>Follow US</li><br></br>
+		<li> <img src="https://cdn-icons-png.flaticon.com/512/37/37409.png?w=740&t=st=1671602413~exp=1671603013~hmac=e2223c669fa836fcc43c026b0adb21dc0ab886a9db1eb23f52a652e33e683e0a" alt="img" height="12px" width="12px"></img>&ensp;Facebook</li><br></br>
+		<li><img src="https://cdn-icons-png.flaticon.com/512/37/37405.png?w=740&t=st=1671602440~exp=1671603040~hmac=88e7e3e6ccdf296cba706fbf8af0db43e5e033449b7ff9610864ab34ae01efff" alt="img" height="12px" width="12px"></img>&ensp;Instagram</li><br></br>
+		<li><img src="https://cdn-icons-png.flaticon.com/512/1051/1051333.png?w=740&t=st=1671602587~exp=1671603187~hmac=53b2d620088402129c29b4b55160e8fbb884dd0b1fc338d38eb96dc3fc505bbd" alt="img" height="12px" width="12px"></img>&ensp;Linkedin</li><br></br>
+		<li><img src="https://cdn-icons-png.flaticon.com/512/39/39552.png?w=740&t=st=1671602333~exp=1671602933~hmac=a740faeb1588c9431205086bef279f7a19cb3493b6260e128dd8de74c640e609" alt="img" height="12px" width="12px"></img>&ensp;Twitter</li><br></br>
+
+</ul></td>
+
+
+
+      </tr>
+      </table>
+      </footer>
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+  );
+}
+
+export default App;
+
---
+++ src/component/test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test(){
+    return(
+    <p>Hello</p>
+
+)
+}
+
+export default Test
+
